5cfc210b75a436aab6d40ab7055ddba4ff0a5b0c
Jason Fuller <123666150+jxfuller1@users.noreply.github.com>
2024-03-05 14:18:10 -0800

    Update SQL_table.py
    sql table update changes

--- SQL_table.py
+++ SQL_table.py
@@ -3,7 +3,113 @@ from typing import List, Dict, Union
 import pandas as pd
 
 
+# basically same as sql_maintable_to_dataframe function, just doens't return a dataframe
+def sql_query_table(sql_path: str, table: str, query: str = None) -> Union[List[tuple], None]:
+    if not query:
+        query = f'SELECT * FROM "{table}"'
+
+    try:
+        conn = sqlite3.connect(sql_path)
+        cursor = conn.cursor()
+        cursor.execute(query)
+
+        result = cursor.fetchall()
+        conn.close()
+    except:
+        result = None
+
+    return result
+
+
+def update_sql_table_cell(sql_path: str, table_name: str, row_index: int, new_value, column_name: str = None, column_index: int = None):
+    # must provide one or the other
+    if not column_name and column_index == None:
+        print("ERROR, must provide column name or column_index")
+
+    if column_index or column_name or column_index == 0:
+        conn = sqlite3.connect(sql_path)
+        cursor = conn.cursor()
+
+        # get column names, this is so that i can then get the column name by index location
+        # since you cant update columns in sqlite3 by index, you need the column name
+        # this also makes it so that the user doesn't have to provide the column name
+        if not column_name:
+            cursor.execute(f'PRAGMA table_info("{table_name}")')
+            columns_info = cursor.fetchall()
+            column_names = [column[1] for column in columns_info]
+            column_name = column_names[column_index]
+
+        # Execute the UPDATE statement
+        query = f'UPDATE "{table_name}" SET "{column_name}" = ? WHERE rowid = ?'
+        cursor.execute(query, (new_value, row_index))
+
+        conn.commit()
+        conn.close()
+
+
+# add row and returns it's rowid
+def add_sql_row(sql_path: str, table_name: str, values: list) -> int:
+    conn = sqlite3.connect(sql_path)
+    cursor = conn.cursor()
+    placeholders = ', '.join(['?' for x in values])
+
+    query = f'INSERT INTO "{table_name}" VALUES ({placeholders})'
+
+    cursor.execute(query, values)
+
+    last_rowid = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return last_rowid
+
+
+def del_sql_row(sql_path, table_name: str, row_index: int):
+    conn = sqlite3.connect(sql_path)
+    cursor = conn.cursor()
+
+    primary_key_column = 'rowid'
+    query = f'DELETE FROM "{table_name}" WHERE {primary_key_column} = ?'
+    cursor.execute(query, (row_index,))
+
+    conn.commit()
+    conn.close()
+
+
+def sql_get_all_rowids(sql_path: str, table_name: str) -> List[int]:
+    conn = sqlite3.connect(sql_path)
+    cursor = conn.cursor()
+    query = f'SELECT rowid FROM "{table_name}"'
+    cursor.execute(query)
+
+    rowids = [row[0] for row in cursor.fetchall()]
+    conn.close()
+
+    return rowids
+
+
+# this function not being used
+def sql_retrieve_rowid_by_index(sql_path, table_name: str, row_index: int) -> tuple:
+    conn = sqlite3.connect(sql_path)
+    cursor = conn.cursor()
+
+    query = f'SELECT rowid FROM "{table_name}" LIMIT 1 OFFSET {row_index}'
+    cursor.execute(query)
+    rowid = cursor.fetchall()
+
+    conn.close()
+
+    return rowid
+
+
 def create_sql_table_from_excel(excel_file_path, table_name, sql_path):
+    if '"' not in table_name[0]:
+        table_name = '"' + table_name
+
+    if '"' not in table_name[-1]:
+        table_name = table_name + '"'
+
     conn = sqlite3.connect(sql_path)
     df = pd.read_excel(excel_file_path)
     df.to_sql(table_name, conn, if_exists='replace', index=False)
@@ -11,9 +117,13 @@ def create_sql_table_from_excel(excel_file_path, table_name, sql_path):
     conn.commit()
     conn.close()
 
+
 def create_blank_sql_table(sql_path: str, table_name: str, column_names: List[str]):
-    query = 'CREATE TABLE IF NOT EXISTS ' + table_name + ' ('
-    query += ', '.join(column_names)
+    # Add single quotes around each column name in case of spaces on column names
+    quoted_column_names = [f'"{column_name}"' for column_name in column_names]
+
+    query = 'CREATE TABLE IF NOT EXISTS ' + '"' + table_name + '"' + ' ('
+    query += ', '.join(quoted_column_names)
     query += ');'
 
     conn = sqlite3.connect(sql_path)
@@ -22,14 +132,16 @@ def create_blank_sql_table(sql_path: str, table_name: str, column_names: List[st
     conn.commit()
     conn.close()
 
+
 def drop_sql_table(sql_path: str, table_name: str):
-    query = f'DROP TABLE IF EXISTS {table_name}'
+    query = f'DROP TABLE IF EXISTS "{table_name}"'
 
     conn = sqlite3.connect(sql_path)
     cursor = conn.cursor()
     cursor.execute(query)
     conn.commit()
     conn.close()
+
 
 # convert passed column to proper datetime format
 def df_datetime_type_conversion(df, column_to_convert_name: List[str], time: bool = False) -> pd.DataFrame:
@@ -67,7 +179,7 @@ def sql_tables(sql_path) -> List[str]:
 
 def sql_maintable_to_dataframe(sql_path: str, table: str, query: str = None) -> Union[pd.DataFrame, None]:
     if not query:
-        query = f"SELECT * FROM {table}"
+        query = f'SELECT * FROM "{table}"'
 
     try:
         conn = sqlite3.connect(sql_path)
@@ -81,6 +193,7 @@ def sql_maintable_to_dataframe(sql_path: str, table: str, query: str = None) -> 
         df = None
 
     return df
+
 
 def sql_subtable_to_dataframe(sql_path: str, allsub_tables: List[str], subtable_name: str,
                               subtable_headers: List[str], query_subtable: str = None) -> Union[pd.DataFrame, None]:
@@ -97,8 +210,8 @@ def sql_subtable_to_dataframe(sql_path: str, allsub_tables: List[str], subtable_
     # this is due to limitations of sqlite3 not being able to handle spaces in column header names
     for index, column in enumerate(headers):
         if ' ' in column:
-            if "'" not in column[0] or "'" not in column[-1]:
-                headers[index] = "'" + column + "'"
+            if '"' not in column[0] or '"' not in column[-1]:
+                headers[index] = '"' + column + '"'
 
     # maintable_index column header must be added for subtables to work
     if "maintable_index" not in headers:
@@ -106,7 +219,7 @@ def sql_subtable_to_dataframe(sql_path: str, allsub_tables: List[str], subtable_
 
     # if table not found in database, create it
     if subtable_name not in allsub_tables:
-        query = 'CREATE TABLE IF NOT EXISTS ' + subtable_name + ' ('
+        query = 'CREATE TABLE IF NOT EXISTS ' + '"' + subtable_name + '"' + ' ('
         query += ', '.join(headers)
         query += ');'
 
@@ -118,9 +231,9 @@ def sql_subtable_to_dataframe(sql_path: str, allsub_tables: List[str], subtable_
 
     # default sql query if none provided
     if not query_subtable:
-        query_subtable = "SELECT "
+        query_subtable = 'SELECT '
         query_subtable += ', '.join(headers)
-        query_subtable += f" FROM {subtable_name}"
+        query_subtable += f' FROM "{subtable_name}"'
 
     try:
         conn = sqlite3.connect(sql_path)
@@ -141,7 +254,7 @@ def subtable_df_to_list(subtable_df: pd.DataFrame, maintable_df: pd.DataFrame) -
 
         # drop the maintable_index, don't want it for displaying on table, this is just a reference for row on the main
         # table it correlates with
-        row_df.drop(columns=["maintable_index"], inplace=True)
+        row_df = row_df.drop(columns=["maintable_index"])
 
         # convert all items in dataframe to stringtype before making lists to prevent any issues when sending to qtableview
         row_df = row_df.astype(str)
@@ -188,41 +301,6 @@ def dataframe_rows_checkboxed(df: pd.DataFrame, columns_with_checkboxes: List[st
     else:
         return None
 
-# COMMENTED OUT CODE PURELY FOR TESTING PURPOSES
-# COMMENTED OUT CODE PURELY FOR TESTING PURPOSES
-# COMMENTED OUT CODE PURELY FOR TESTING PURPOSES
-#create_sql_table_from_excel(r"C:\Users\jxful\OneDrive\Desktop\sql\table_database.xls", "K057", r'C:\Users\jxful\OneDrive\Desktop\sql\maintables.db')
-
-
-#    table_names = sql_tables(r'C:\Users\jxful\OneDrive\Desktop\sql\maintables.db')
-#    #print(table_names)
-    
-#table = "K057"
-#df = sql_maintable_to_dataframe(r'\\NAS3\Users\Jason Fuller\Desktop\tables\maintables.db', table)
-#print(df)
-#for index, row in df.iterrows():
-#    print(row.to_list())
-    
-#    if df:
-#        maintable_df = df_datetime_type_conversion(df, column_to_convert_name=["Date"])
-#        checked_rows = dataframe_rows_checkboxed(maintable_df, ['Conformed', "N/A", "Have", "POA"])
-#        maintable_data = maintable_df_to_list(maintable_df)
-    
-#allsub_tables = sql_tables(r"\\NAS3\Users\Jason Fuller\Desktop\tables\subtables.db")
-#print(allsub_tables)
-#subtable_df = sql_subtable_to_dataframe(r"\\NAS3\Users\Jason Fuller\Desktop\tables\subtables.db", allsub_tables, "K057_subtable", ["'NCR No.'", "Disposition", "Date", "Extra", "Completed"])
-#print(subtable_df)
-
-#    if subtable_df and df:
-#        subtable_data_list = subtable_df_to_list(subtable_df, df)
-
-
-# what parameters i need to send
-#  maintable SQL path, main table name, datetime column name if datetime option being used, column names for checkboxes if checkboxes option being used,
-# subtable SQL path if subtables option chosen, all table names from SQL .db file, sub-table name, sub-table column headers
-
-# what i need it to return
-
 
 def collect_maintabledata_fromSQL_databases(main_df: pd.DataFrame, maintable_datetime_columnname: List[str] = None,
                                             maintable_checkbox_columnnames: List[str] = None) -> \
@@ -239,9 +317,22 @@ def collect_maintabledata_fromSQL_databases(main_df: pd.DataFrame, maintable_dat
     return maintable_data, checked_rows
 
 
-def collect_subtabledata_fromSQL_database(sub_df: pd.DataFrame, sql_subtable_path: str, allsub_tables: List[str], sql_subtable_name: str,
-                                          sub_table_headers_labels: List[str], sql_subtable_query: str = None):
+# COMMENTED OUT CODE PURELY FOR TESTING PURPOSES
+# COMMENTED OUT CODE PURELY FOR TESTING PURPOSES
+# COMMENTED OUT CODE PURELY FOR TESTING PURPOSES
 
-   # def subtable_df_to_list(subtable_df: pd.DataFrame, maintable_df: pd.DataFrame) -> List[List[str]]:
+#sql_maintable_name = "K057"
+#sql_maintable_path = r"\\NAS3\Users\Jason Fuller\Desktop\tables\maintables.db"
+#row_index = 1
+#column_index = 0
+#value = "Something"
 
-    pass
+
+#drop_sql_table(r"\\NAS3\Users\Jason Fuller\Desktop\tables\subtables.db", "K057_subtable")
+#test = sql_query_table(r"\\NAS3\Users\Jason Fuller\Desktop\tables\subtables.db", "K057_subtable")
+
+#update_sql_table_cell(sql_maintable_path, sql_maintable_name, row_index, value, column_index=column_index)
+
+
+#df = sql_maintable_to_dataframe(sql_maintable_path, sql_maintable_name)
+#print(df)
